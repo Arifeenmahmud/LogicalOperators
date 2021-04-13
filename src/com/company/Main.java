e4b37b25b92979b24f63a9154ce2b2fba03320bc
ArifeenMahmud <arifeen.mahmud@yahoo.com>
2021-04-14 04:47:48 +0600

    Logical Operators

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,36 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        boolean isScreenBig = false;
+        boolean isColorOfThePhoneBlack = true;
+        boolean isRamEnough = true;
+        /*----------&&---------*/
+            // false   && true => false
+            //true and true =>
+            //false and false = false
+
+        System.out.println("&&");
+        if(isScreenBig && isColorOfThePhoneBlack){
+            System.out.println("I want that Phone");
+
+        }else{
+            System.out.println("I don't Want that Phone");
+
+        }
+        System.out.println("||");
+
+        // true || true => true
+        // true || false => true
+        // false || false =>false
+
+        if((isScreenBig || isColorOfThePhoneBlack) && isRamEnough){
+            System.out.println("I want that Phone");
+
+        }else{
+            System.out.println("I don't Want that Phone");
+
+        }
+
+
     }
 }
